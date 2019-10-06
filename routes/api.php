b870dcaee754e64d8a652d5f4ec61f568adb4fa8
Santiagomrn <santiagomrn98@gmail.com>
2019-10-05 23:05:23 -0500

    ready esp32

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,8 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+route::post('/Esp32','Esp32Controller@store');
+route::get('/Esp32','Esp32Controller@index');
+Route::get('/Esp32/{id}','Esp32Controller@show')->name('showEsp32');
+Route::put('/Esp32/{id}','Esp32Controller@update')->name('updateEsp32');
--- routes/web.php
+++ routes/web.php
@@ -17,8 +17,5 @@ Route::get('/', function () {
 Route::get('/Esp32/config', function () {
     return view('ESP32');
 });
-route::post('/Esp32','Esp32Controller@store');
-route::get('/Esp32','Esp32Controller@index');
-Route::get('/Esp32/{id}','Esp32Controller@show')->name('showEsp32');
-Route::put('/Esp32/{id}','Esp32Controller@update')->name('updateEsp32');
+
 
